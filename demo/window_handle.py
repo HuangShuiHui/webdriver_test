7c81d1001ab907728179c0503432b703ab635999
huangshuihui <shuihui_huang@163.com>
2018-03-12 18:30:27 +0800

    窗口句柄操作 通过调用js的方法实现在新的标签页打开链接

--- demo/window_handle.py
+++ demo/window_handle.py
@@ -17,10 +17,15 @@ dr.implicitly_wait(5)
 
 
 navs = dr.find_elements_by_class_name('mnav')
+
+
 # 获取当前窗口句柄
+h = dr.current_window_handle
 for nav in navs:
-    nav.click()
-    h = dr.current_window_handle
+    #nav.click()
+    href = nav.get_attribute('href')
+    print(href)
+    dr.execute_script("window.open('%s')" % href)
     handles = dr.window_handles
     print len(handles)
     for handle in handles:
@@ -29,6 +34,6 @@ for nav in navs:
             print(dr.title)
             sleep(3)
             dr.close()
-    dr.back()
+    dr.switch_to.window(h)
 print('end')
 dr.quit()
